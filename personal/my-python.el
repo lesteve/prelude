16fa3d31c38af60e2e90a2928020a6161580052c
Loïc Estève <loic.esteve@ymail.com>
2015-11-17 09:10:19 +0100

    Synchronised with desktop

--- personal/my-python.el
+++ personal/my-python.el
@@ -6,3 +6,32 @@
 
 ;; Use ipython rather than plain python for the python shell
 (elpy-use-ipython)
+
+;; Work around Anaconda having a different Qt version that my system Qt
+(setq python-shell-interpreter-args "-i --matplotlib tk")
+
+;; emacs ipython notebook
+(require 'ein)
+
+;; auto-complete for ein
+(setq ein:use-auto-complete-superpack t)
+;; (setq ein:use-smartrep t)
+
+;; from the doc
+(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+
+;; Taken from
+;; https://github.com/millejoh/emacs-ipython-notebook/blob/master/lisp/zeroein.el
+;; This significantly improves completion in the ipython notebook
+;; buffers. Otherwise . opens a minibuffer to do the completion which
+;; is really crap
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; MuMaMo (not sure about this one)
+(custom-set-faces
+ '(mumamo-background-chunk-major
+   ((((class color) (min-colors 88) (background dark)) nil)))
+ ;; '(mumamo-background-chunk-submode1
+ ;;   ((((class color) (min-colors 88) (background dark)) nil)))
+ )
