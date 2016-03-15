4ae62ed2342a61cf73d870360d8aea5ccb46e758
Loïc Estève <loic.esteve@ymail.com>
2016-03-15 15:20:20 +0100

    Fix startup hanging at loading recentf for 20 seconds

---
+++ personal/preload/slow-startup-tramp-fix.el
@@ -0,0 +1,5 @@
+;; Fix slow startup, stuck for ages at Loading recentf. For more
+;; details, see:
+;; https://github.com/bbatsov/prelude/issues/896
+;; https://github.com/emacs-helm/helm/issues/1000
+(setq tramp-ssh-controlmaster-options)
