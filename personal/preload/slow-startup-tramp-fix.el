8eba36654b12a5afed5940f23efde897410ad7f4
Loïc Estève <loic.esteve@ymail.com>
2017-07-19 14:01:02 +0200

    Remove slow-startup-tramp-fix.el.
    Looks like the work-around is not needed any more.

--- personal/preload/slow-startup-tramp-fix.el
+++
@@ -1,5 +0,0 @@
-;; Fix slow startup, stuck for ages at Loading recentf. For more
-;; details, see:
-;; https://github.com/bbatsov/prelude/issues/896
-;; https://github.com/emacs-helm/helm/issues/1000
-(setq tramp-ssh-controlmaster-options)
