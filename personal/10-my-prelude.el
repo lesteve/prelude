ad9e99b40ce13c4298acf5ed4bcd6eb6c6554a95
Loïc Estève <loic.esteve@ymail.com>
2018-04-11 17:48:22 +0200

    Load some prelude modules by default

---
+++ personal/10-my-prelude.el
@@ -0,0 +1,37 @@
+;;; A subset of https://github.com/bbatsov/prelude/blob/master/sample/prelude-modules.el
+
+(require 'prelude-ivy) ;; A mighty modern alternative to ido
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company) ;; Completion framework
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
+;; (require 'prelude-evil) ;; Maybe one day?
+
+;;; Programming languages support
+(require 'prelude-c)
+;; (require 'prelude-clojure)
+;; (require 'prelude-coffee)
+;; (require 'prelude-common-lisp)
+(require 'prelude-css)
+(require 'prelude-emacs-lisp)
+;; (require 'prelude-erlang)
+;; (require 'prelude-elixir)
+;; (require 'prelude-go)
+;; (require 'prelude-haskell)
+(require 'prelude-js)
+(require 'prelude-latex)
+;; (require 'prelude-lisp)
+;; (require 'prelude-ocaml)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+;; (require 'prelude-perl)
+(require 'prelude-python)
+;; (require 'prelude-ruby)
+;; (require 'prelude-rust)
+;; (require 'prelude-scala)
+;; (require 'prelude-scheme)
+(require 'prelude-shell)
+;; (require 'prelude-scss)
+(require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+(require 'prelude-yaml)
+
--- personal/my-prelude.el
+++
@@ -1,1 +0,0 @@
-(require 'prelude-helm-everywhere)
